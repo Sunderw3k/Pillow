Sunderw3k/Pillow

commit 6967a4bd70bdb754eb768f9fb88ab039e4da04b1

fix: use correct `Set` implementation in `PurchasedScriptIdsResp`

server/src/main/kotlin/rip/sunrise/server/netty/ServerHandler.kt
@@ -94,7 +94,9 @@ class ServerHandler(private val config: Config, private val http: JarHttpServer)
 
             is AuthenticationCodeRequest -> ctx.writeAndFlush(AuthenticationCodeResp(AUTHENTICATION_CODE))
 
-            is PurchasedScriptIdsRequest -> ctx.writeAndFlush(PurchasedScriptIdsResp(config.purchasedScripts.map { it.storeId }.toSet()))
+            is PurchasedScriptIdsRequest -> ctx.writeAndFlush(PurchasedScriptIdsResp(TreeSet<Int>().apply {
+                addAll(config.purchasedScripts.map { it.storeId })
+            }))
 
             is a5 -> ctx.writeAndFlush(am(0))
 

packets/src/main/kotlin/rip/sunrise/packets/clientbound/PurchasedScriptIdsResp.kt
@@ -1,5 +1,6 @@
 package rip.sunrise.packets.clientbound
 
 import java.io.Serializable
+import java.util.TreeSet
 
-data class PurchasedScriptIdsResp(val p: Set<Int>) : Serializable
+data class PurchasedScriptIdsResp(val p: TreeSet<Int>) : Serializable

commit e3c67dcbbd61c136022357ea8bf76a708cd69a3a

fix: add missing import caused by 6967a4bd

server/src/main/kotlin/rip/sunrise/server/netty/ServerHandler.kt
@@ -25,6 +25,7 @@ import java.util.Base64
 import rip.sunrise.server.logger
 import rip.sunrise.server.utils.extensions.generateFakeScriptWrappers
 import rip.sunrise.server.utils.extensions.isRealScriptId
+import java.util.TreeSet
 
 const val ACCOUNT_SESSION_ID = "cMU/vYTQnRyD2cFx1i1J6aa+ZpRIINh5qkMxoTh8XoA"
 const val SCRIPT_SESSION_ID = "dbsVbKA4mRLE4NaOMXCCnvPYEJsNsXdwek6hosbCiQ0"